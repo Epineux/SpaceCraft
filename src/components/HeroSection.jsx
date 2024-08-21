06eb043c8b3c719dda5bc48c9011e011a7478cbc
Epineux <martinlecontehelloworld@gmail.com.noreply.github.com>
2024-08-21 16:43:38 +0900

    Update Hero Section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,46 +6,46 @@ const HeroSection = () => {
   return (
     // Hero flex container
     <div>
-      <div className="flex flex-col items-center bg-[url('/assets/images/heroImage.jpg')] bg-cover bg-center h-[calc(100vh-72px)]">
-        {/* Text part */}
+      {/* Text and Hero image part (100vh*/}
+      <div className="flex flex-col items-center justify-evenly px-space-l py-10 bg-[url('/assets/images/heroImage.jpg')] bg-cover bg-center h-[calc(100vh-60px)]">
         <h1 className="text-step-5 text-center tracking-wide">
           Spaceship Kits for <br />
           <span className="font-bold bg-gradient-to-r from-yellow-400 to-yellow-800 text-transparent bg-clip-text">
             Galactic Explorers
           </span>
         </h1>
-        <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
+        <p className="text-step-0 text-center text-neutral-400">
           Embark on an interstellar adventure with SpaceCraft, your online shop
           for premium spaceship components.
         </p>
         {/* Button part */}
-        <div className="flex justify-center my-10">
+        <div className="flex gap-space-m">
           <a
             href="#"
             className="bg-gradient-to-r from-yellow-500 to-yellow-800  px-4 py-3 rounded-md"
           >
             Take Off
           </a>
-          <a href="#" className="mx-3 px-4 py-3 rounded-md border">
+          <a href="#" className="px-4 py-3 rounded-md border">
             Explore
           </a>
         </div>
       </div>
       {/* Video part */}
-      <div className="flex mt-10 justify-center">
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] py-20 px-space-m gap-space-m">
         <video
           src={video1}
           autoPlay
           loop
           muted
-          className="rounded-lg w-1/2 md:w-2/5 mx-4 my-4 border border-yellow-500"
+          className="rounded-lg border border-yellow-500"
         ></video>
         <video
           src={video2}
           autoPlay
           loop
           muted
-          className="rounded-lg w-1/2 md:w-2/5  mx-4 my-4 border border-yellow-500"
+          className="rounded-lg border border-yellow-500"
         ></video>
       </div>
     </div>
